f4223296e06c2b6e8bcb3ee29f7741357f7a613c
samekshya <samikshyabaniya003@gmail.com>
2025-03-03 14:00:28 +0545

    Middleware uploads

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Store files in `uploads/` folder
+    cb(null, "uploads/"); 
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
